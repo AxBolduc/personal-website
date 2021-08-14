c038c520f68c24cde49dd257966568403fea7d58
Alex Bolduc <aebolduc@wpi.edu>
2021-08-14 17:56:14 -0400

    Add Images

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,18 @@
 import Head from "next/head";
+import AboutPane from "../components/AboutPane";
 import Header from "../components/Header";
 import HeroPane from "../components/HeroPane";
 
 export default function Home() {
 	return (
-		<div className="px-48">
+		<div className="">
 			<Head>
 				<title>Alex Bolduc - Software Engineer</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<HeroPane />
+			<AboutPane />
 		</div>
 	);
 }
